854345b09042828f93d77cad88857d6d2aae53ce
Tahreem Altaf <tahreemaltaf5@gmail.com>
2025-12-17 08:57:56 +0500

    Switch to us-east-1 and dynamic AMI to bypass vCPU limit

--- main.tf
+++ main.tf
@@ -220,9 +220,25 @@ HTML
   EOF
 }
 
+# Get latest Ubuntu 22.04 AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 # EC2 Instance
 resource "aws_instance" "web_server" {
-  ami           = var.ami_id
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   key_name      = aws_key_pair.deployer.key_name
 
--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@
 variable "aws_region" {
   description = "AWS region for resources"
   type        = string
-  default     = "ap-south-1"
+  default     = "us-east-1"
 }
 
 # EC2 Instance type variable
